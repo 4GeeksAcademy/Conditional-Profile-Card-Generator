40b39cb286f828bd721cabb5169ff2bda49e1e76
AntonioMora <126864866+Antoniomorales17@users.noreply.github.com>
2023-05-24 10:07:09 +0000

    last commit

--- src/js/app.js
+++ src/js/app.js
@@ -33,20 +33,39 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-
-          <h1>${variables.name} ${variables.lastname}</h1>
-          <h2>${variables.role} </h2>
-          <h3>${variables.city} ${variables.country}</h3>
-          <ul class="position-right">
-          <li><a href="https://twitter.com/${variables.twitter}"><i class="bi bi-twitter"></i></a></li>
-          
-          
-            <li><a href="https://github.com/${variables.github}"><i class="bi bi-github"></i></a></li>
-            <li><a href="https://linkedin.com/${variables.linkedin}"><i class="bi bi-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/${variables.instagram}"><i class="bi bi-instagram"></i></a></li>
+          <h1>${variables.name ? variables.name : "Name"}
+              ${variables.lastname ? variables.lastname : "Last-Name"}</h1>
+          <h2>${variables.role ? variables.role : "Role"}</h2>
+          <h3>${variables.city ? variables.city : "Miami"} 
+              ${variables.country ? variables.country : "USA"}</h3>
+            <ul class= ${
+              variables.socialMediaPosition
+                ? variables.socialMediaPosition
+                : "position-left"
+            } />
+            ${
+              variables.twitter != null
+                ? '<li><a href="https://twitter.com/4geeksacademy"><i class="bi bi-twitter"></i></a></li>'
+                : " "
+            }  
+            ${
+              variables.linkedin != null
+                ? '<li><a href="https://linkedin.com/4geeksacademy"><i class="bi bi-linkedin"></i></a></li>'
+                : " "
+            } 
+            ${
+              variables.instagram != null
+                ? '<li><a href="https://instagram.com/4geeksacademy"><i class="bi bi-instagram"></i></a></li>'
+                : " "
+            }
+            ${
+              variables.github != null
+                ? '<li><a href="https://github.com/4geeksacademy"><i class="bi bi-github"></i></a></li>'
+                : "  "
+            }   
           </ul>
         </div>
-    `;
+      `;
 }
 
 /**
